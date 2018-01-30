f206f38b7cc91cd5d7a7dda2bd4068a983d8338c
alemk <alemseged.hailu@gmail.com>
2018-01-30 18:07:25 -0500

    another commit

--- src/main/java/com/toyota/model/MainController.java
+++ src/main/java/com/toyota/model/MainController.java
@@ -3,33 +3,35 @@ package com.toyota.model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class MainController
 {
     @RequestMapping("/car")
-    public String cardisplay(HttpServletRequest parm, HttpServletRequest parm2,HttpServletRequest parm3)
+    public String cardisplay(HttpServletRequest parm)
     {
         Car v = new Car();
+        int accele ;
         String color = parm.getParameter("carcolor");
         v.setColor(color);
-        String model = parm2.getParameter("model");
+        String model = parm.getParameter("model");
         v.setModel(model);
+        String speed = parm.getParameter("speed");
+        v.setSpeed(speed);
+        accele = Integer.parseInt(speed);
+        if (accele > 65){v.setAccelerate("is accelerating");}
+        else {v.setAccelerate("is not accelerating");}
 
-        v.setAccelerate();
-        String start = v.start();
-        String stop = v.stop();
-        // System.out.println(v.getColor());
+
+        return "The " + v.getColor() + " " + v.getModel() + " is " + v.start() + "<br/>" +
+               "The " + v.getColor() + " " + v.getModel() + " is going  " + v.getSpeed()+ " mph" + "<br/>" +
+               "The " + v.getColor() + " " + v.getModel() + " is going  " + v.getAccelerate()+ "<br/>" +
+               "The " + v.getColor() + " " + v.getModel() + " is " + v.stop();
+
+        // return v.start() + v.getColor() +v.getModel() + "is going " + v.getSpeed() + v.stop();
 
     }
 
-        @Override
-        public String toString() {
-        return + "Car{" +
-                "color='" + color + '\'' +
-                '}';
-    }
 }
 
